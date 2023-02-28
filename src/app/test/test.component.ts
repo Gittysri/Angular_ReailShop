99958add0315d177587930eeb6fc4494285b5476
Srinath <srinath.ka16@gmail.com>
2023-02-28 11:26:27 +0530

    Added Images and Product list json in assets

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.sass']
+})
+export class TestComponent {
+  public name = "Srinath";
+
+  greetUser(){
+    return "Hi " + this.name;
+  }
+
+}
